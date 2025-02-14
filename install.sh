f425f481cd646321a577ed3ddd03d342580f6122
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-02-14 21:17:06 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,13 @@
 
 export GIT_USERNAME=${GIT_USERNAME}
 export GIT_PAT=${GIT_PAT}
+export BRANCH_NAME=${BRANCH_NAME}
 
-if [ -z "$GIT_USERNAME" ] || [ -z "$GIT_PAT" ]; then
-  echo "Missing environment variables: GIT_USERNAME or GIT_PAT"
+if [ -z "$GIT_USERNAME" ] || [ -z "$GIT_PAT" ] || [ -z "$BRANCH_NAME" ]; then
+  echo "Missing environment variables: GIT_USERNAME, GIT_PAT or BRANCH_NAME"
   exit 1
 fi
 
-git clone https://$GIT_USERNAME:$GIT_PAT@github.com/bichhawat/dolev-yao-star.git /home/build/dolev-yao-star/
+git clone --branch $BRANCH_NAME --single-branch https://$GIT_USERNAME:$GIT_PAT@github.com/bichhawat/dolev-yao-star.git /home/build/dolev-yao-star/
 cd /home/build/dolev-yao-star/ && clear
 exec bash
