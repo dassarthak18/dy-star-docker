995ac370bcd9847ac0a8627403e6c949f6b333de
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-02-14 23:16:06 +0530

    added $REPO_NAME variable for personal use

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 export GIT_USERNAME=${GIT_USERNAME}
 export GIT_PAT=${GIT_PAT}
+export REPO_NAME=${REPO_NAME}
 export BRANCH_NAME=${BRANCH_NAME}
 
 if [ -z "$GIT_USERNAME" ] || [ -z "$GIT_PAT" ]; then
@@ -9,8 +10,9 @@ if [ -z "$GIT_USERNAME" ] || [ -z "$GIT_PAT" ]; then
   exit 1
 fi
 
+REPO_NAME=${REPO_NAME:-github.com/bichhawat/dolev-yao-star.git}
 BRANCH_NAME=${BRANCH_NAME:-master}
 
-git clone --branch $BRANCH_NAME --single-branch https://$GIT_USERNAME:$GIT_PAT@github.com/bichhawat/dolev-yao-star.git /home/build/dolev-yao-star/
+git clone --branch $BRANCH_NAME --single-branch https://$GIT_USERNAME:$GIT_PAT@$REPO_NAME /home/build/dolev-yao-star/
 cd /home/build/dolev-yao-star/ && clear
 exec bash
